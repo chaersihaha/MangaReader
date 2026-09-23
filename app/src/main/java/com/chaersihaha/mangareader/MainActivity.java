4741031ad83a25ab4e9fe623e130f032bbf9c730
chaersihaha <2929813135@qq.com>
2026-09-23 12:37:30 +0800

    Update MainActivity.java

--- app/src/main/java/com/chaersihaha/mangareader/MainActivity.java
+++ app/src/main/java/com/chaersihaha/mangareader/MainActivity.java
@@ -6,7 +6,6 @@ import android.graphics.Color;
 import android.graphics.Typeface;
 import android.graphics.drawable.GradientDrawable;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -68,40 +67,7 @@ public class MainActivity extends Activity {
         return (int) (value * getResources().getDisplayMetrics().density + 0.5f);
     }
 
-    private void fullscreen() {
-
-        if (Build.VERSION.SDK_INT >= 30) {
-
-            getWindow().setDecorFitsSystemWindows(false);
-
-            if (getWindow().getInsetsController() != null) {
-
-                getWindow().getInsetsController().hide(
-                        android.view.WindowInsets.Type.statusBars()
-                                | android.view.WindowInsets.Type.navigationBars()
-                );
-
-                getWindow().getInsetsController().setSystemBarsBehavior(
-                        android.view.WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
-                );
-            }
-
-        } else {
-
-            getWindow().getDecorView().setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-            );
-        }
-    }
-
     private void base() {
-
-        fullscreen();
 
         root = new LinearLayout(this);
 
@@ -754,7 +720,7 @@ public class MainActivity extends Activity {
                     View.DragShadowBuilder shadow =
                             new View.DragShadowBuilder(v);
 
-                    if (Build.VERSION.SDK_INT >= 24) {
+                    if (android.os.Build.VERSION.SDK_INT >= 24) {
 
                         v.startDragAndDrop(
                                 null,
